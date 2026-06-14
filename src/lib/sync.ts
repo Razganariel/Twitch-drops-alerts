ac61e5e25e3277994173ad8aac158e1fa8a9eaed
dvergar <dvergar@hotmail.fr>
2026-06-14 09:23:10 +0200

    add datetime in log

--- src/lib/sync.ts
+++ src/lib/sync.ts
@@ -1,3 +1,4 @@
+import "./logger"
 import { prisma } from "@/lib/prisma"
 import { getActiveDropCampaigns, getDropCampaignDetails } from "@/services/twitch"
 import { getSyncGqlToken } from "@/services/sync-account"
